fcfb2e497c3072fd959cabb460df4122affc0815
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-21 23:26:21 +0200

    feat: displaying tiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use catan_tui::{hex::Hex, number::Number, terrain::Terrain};
+
 fn main() {
-    println!("Hello, world!");
+    let hex = Hex {
+        terrain: Terrain::Desert,
+        number: Number::_2
+    };
+    println!("{}", hex);
+    let hex = Hex {
+        terrain: Terrain::Fields,
+        number: Number::_2
+    };
+    println!("{}", hex);
+    let hex = Hex {
+        terrain: Terrain::Forest,
+        number: Number::_2
+    };
+    println!("{}", hex);
+    let hex = Hex {
+        terrain: Terrain::Hills,
+        number: Number::_2
+    };
+    println!("{}", hex);
+    let hex = Hex {
+        terrain: Terrain::Mountains,
+        number: Number::_2
+    };
+    println!("{}", hex);
+    let hex = Hex {
+        terrain: Terrain::Pasture,
+        number: Number::_2
+    };
+    println!("{}", hex);
 }
