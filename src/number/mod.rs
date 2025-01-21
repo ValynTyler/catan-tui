dd176aa4ab8f1181988f111ea13389cfbf904128
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-21 23:26:03 +0200

    feat: impl display for number

--- src/number/mod.rs
+++ src/number/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub enum Number {
     _2,
     _3,
@@ -10,4 +12,22 @@ pub enum Number {
     _10,
     _11,
     _12,
+}
+
+impl Display for Number {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Number::_2 => write!(f, "02"),
+            Number::_3 => write!(f, "03"),
+            Number::_4 => write!(f, "04"),
+            Number::_5 => write!(f, "05"),
+            Number::_6 => write!(f, "06"),
+            Number::_7 => write!(f, "07"),
+            Number::_8 => write!(f, "08"),
+            Number::_9 => write!(f, "09"),
+            Number::_10 => write!(f, "10"),
+            Number::_11 => write!(f, "11"),
+            Number::_12 => write!(f, "12"),
+        }
+    }
 }
