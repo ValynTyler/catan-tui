0d935d0ca78637a317f085364d28f0fef5df71f8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-01-21 23:26:13 +0200

    feat: impl display for hex

--- src/hex/mod.rs
+++ src/hex/mod.rs
@@ -1,7 +1,49 @@
+use std::fmt::Display;
+use std::fmt::Write;
+
+use rand::Rng;
+use colored::Colorize;
+
 use crate::number::*;
 use crate::terrain::*;
 
 pub struct Hex {
     pub terrain: Terrain,
     pub number: Number,
+}
+
+impl Display for Hex {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut s = String::new();
+
+        writeln!(&mut s, "  x----x  ")?;
+        writeln!(&mut s, " /......\\ ")?;
+        writeln!(&mut s, "x...{}...x", self.number)?;
+        writeln!(&mut s, " \\....../ ")?;
+        writeln!(&mut s, "  x----x  ")?;
+
+        write!(f, "{}", s
+            .chars()
+            .map(|c| if c == '.' {
+                let (option_a, option_b) = match self.terrain {
+                    Terrain::Desert => (".".white(), ".".white()),
+                    Terrain::Fields => ("󱔐".yellow(), "󱪲".bright_yellow()),
+                    Terrain::Forest => ("".green(), "󰔱".bright_green()),
+                    Terrain::Hills => ("󰟾".bright_red(), "".red()),
+                    Terrain::Mountains => ("".bright_black(), "󱉏".white()),
+                    Terrain::Pasture => ("󱔐".bright_green(), "󰳆".bright_white()),
+                };
+
+                let num = rand::thread_rng().gen_range(0..7);
+                if num > 5 {
+                    option_a
+                } else if num > 4 {
+                    option_b
+                } else {
+                    ".".white()
+                }.to_string()
+            } else { c.to_string() } )
+            .collect::<String>()
+        )
+    }
 }
